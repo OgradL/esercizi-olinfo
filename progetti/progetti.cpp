df2bfb485e2c2af5e2f0d784948685ac9bd28288
OgradL <dragoleonardo1@gmail.com>
2024-04-09 19:24:51 +0200

    progetti

--- progetti/progetti.cpp
+++ progetti/progetti.cpp
@@ -1,13 +1,99 @@
 #include <iostream>
+#include <vector>
+#include <assert.h>
+using namespace std;
 
-constexpr const long long MOD = 1e9 + 7;
+template<typename T = long long>
+struct mat{
+	int x, y;
+	T **entries;
+	mat(int sx, int sy, T** values = NULL){
+		x = sx;
+		y = sy;
+		entries = (T**) malloc(x * sizeof(T*));
+		for (int i = 0; i < x; i++){
+			entries[i] = (T*) malloc(y * sizeof(T));
+		}
+		if (values != NULL){
+			for (int i = 0; i < x; i++){
+				for (int j = 0; j < y; j++){
+					entries[i][j] = values[i][j];
+				}
+			}
+		}
+	}
+	mat(int sx, int sy, initializer_list<vector<long long>> values){
+		x = sx;
+		y = sy;
+		entries = (T**) malloc(x * sizeof(T*));
+		vector<vector<long long>> tmp;
+		tmp.insert(tmp.begin(), values);
+		for (int i = 0; i < x; i++){
+			entries[i] = (T*) malloc(y * sizeof(T));
+		}
+		for (int i = 0; i < x; i++){
+			for (int j = 0; j < y; j++){
+				entries[i][j] = tmp[i][j];
+			}
+		}
+	}
+	T* operator [] (int idx){
+		return entries[idx];
+	}
+	mat operator %(long long mod){
+		mat ans = mat<T>(x, y, entries);
+		for (int i = 0; i < x; i++){
+			for (int j = 0; j < y; j++){
+				ans[i][j] %= mod;
+			}
+		}
+		return ans;
+	}
+	mat operator *(mat<T>& a){
+		assert(x == a.y);
+		mat ans = mat<T>(a.x, y);
+		for (int i = 0; i < y; i++){
+			for (int j = 0; j < a.x; j++){
+				ans[j][i] = 0;
+				for (int k = 0; k < x; k++){
+					ans[j][i] += entries[k][i] * a[j][k];
+				}
+			}
+		}
+		return ans;
+	}
+	void set(T v){
+		for (int i = 0; i < x; i++){
+			for (int j = 0; j < y; j++){
+				entries[i][j] = v;
+			}
+		}
+	}
+};
 
-long long POW(long long b, long long e){
-	if (e == 0) return 1;
-	long long v = POW(b, e/2);
-	return (v * v % MOD) * (e&1?b:1) % MOD;
+
+template<typename T>
+T POW(T b, long long e, long long m){
+	if (e == 1) return b;
+	T v = POW(b, e/2, m);
+	v = (v * v) % m;
+	if (e&1){
+		v = (v * b) % m;
+	}
+	return v;
 }
 
+const long long MOD = 1e9 + 7;
+
 int progetti(int N, int K){
-	
+
+	mat X = mat<long long>(5, 5, {{0, 1, 0, 0, 0},
+								  {0, 0, 1, 0, 0},
+								  {0, 0, 0, 1, 0},
+								  {0, 0, 0, 0, 1},
+								  {K>=5, K>=4, K>=3, K>=2, K>=1}});
+
+	mat<long long> ans = POW<mat<long long>>(X, N, MOD);
+
+	return ans[4][4];
 }
