1e2a7ce78ecc81bf3d3682ddc2cf45a0151dc724
OgradL <dragoleonardo1@gmail.com>
2024-04-14 23:00:34 +0200

    fractal + bugged gita

--- gita/gita.cpp
+++ gita/gita.cpp
@@ -83,3 +83,20 @@ int visita(int N, vector<int> &A, vector<int> &B, vector<int> &dolcezza, vector<
 
 	return ans;
 }
+
+int main(){
+	int N;
+	cin >> N;
+	vector<int> dolcezza(N), C(N), A(N - 1), B(N - 1);
+	for (int i = 0; i < N; i++) {
+		cin >> dolcezza[i];
+	}
+	for (int i = 0; i < N; i++) {
+		cin >> C[i];
+	}
+	for (int i = 0; i < N - 1; i++) {
+		cin >> A[i] >> B[i];
+	}
+	cout << visita(N, A, B, dolcezza, C) << endl;
+	return 0;
+}
