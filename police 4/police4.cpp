a57f134680b390794540159b2adcf2db94527e5e
OgradL <dragoleonardo1@gmail.com>
2024-12-07 18:26:57 +0100

    police 4

--- police 4/police4.cpp
+++ police 4/police4.cpp
@@ -3,55 +3,46 @@
 #include <vector>
 using namespace std;
 
-#define INF 1e9+10
+int main(){
 
-int main(int argc, char** argv){
-
-	// if (argc >= 2){
-	// 	freopen(argv[1], "r", stdin);
-	// }
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
 
 	long long N, R, T, L;
 	cin >> N >> R >> T >> L;
 
-	vector<int> X(N);
-	for (int &i: X) cin >> i;
+	vector<long long> X(N);
+	for (long long& i: X) cin >> i;
 	X.push_back(L);
 
-	vector<vector<int>> memo(N + 1, vector<int>(R + 2, INF));
+	vector<vector<long long>> dp(N + 1, vector<long long>(R + 2, 1e18));
 
-	memo[0][0] = X[0];
+	dp[0][0] = X[0];
 
 	for (int i = 1; i <= N; i++){
 		for (int j = 0; j <= R; j++){
-			if (memo[i-1][j] != INF){
-				int time = memo[i-1][j];
-				int stop_time = 0;
+			if (dp[i-1][j] != 1e18){
+				long long time = dp[i-1][j];
+				long long stop_time = 0;
 				if ((time / T) % 2){
 					stop_time = time % T;
 					stop_time = T - stop_time;
 				}
-				memo[i][j] = min(memo[i][j], time + stop_time + X[i] - X[i-1]);
-				// cout << time << " " << (time % stop_time) << " " << stop_time << " " << memo[i][j] << " <----\n";
+				dp[i][j] = min(dp[i][j], time + stop_time + X[i] - X[i-1]);
 
 				if (j <= R && stop_time > 0)
-					memo[i][j+1] = min(memo[i][j+1], time + X[i] - X[i-1]);
-				// cout << memo[i][j] << " ";
+					dp[i][j+1] = min(dp[i][j+1], time + X[i] - X[i-1]);
 			} else {
-				// cout << memo[i][j] << " ";
 				break;
 			}
 		}
-		// cout << "\n";
 	}
 
 	
-
-	int ans = INF;
-	// cout << "ANS: \n";
+	long long ans = 1e18;
 	for (int i = 0; i <= R; i++){
-		// cout << memo[N][i] << "\n";
-		ans = min(ans, memo[N][i]);
+		ans = min(ans, dp[N][i]);
 	}
 
 	cout << ans << "\n";
