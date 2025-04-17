c5d6bd37c0d3d25e861a76c93201b35d7e3764fb
OgradL <dragoleonardo1@gmail.com>
2025-04-17 21:48:38 +0200

    armadio

--- armadio/armadio.cpp
+++ armadio/armadio.cpp
@@ -2,25 +2,47 @@
 #include <vector>
 using namespace std;
 
-int nd(int x){
-	int ans = 1;
-	for (int i = 2; i < x / 2; i++){
-		ans += !(x%i);
-	}
-	return ans;
-}
-
+const int MAXN = 4000001;
 void evadi(int Q, vector<int>& N){
 
-	for (int i = 0; i < Q; i++){
-		int ans = 0;
-		for (int g = 1; g <= N[i]; g++){
-			if (N[i] % g == 0){
-				int d = N[i]/g - 2 * nd(N[i]/g - 1) - (((N[i]/g - 1) % 2 == 0) && ((N[i]/g - 1) > 2));
-				cout << N[i] << " " << g << " " << d << "\n";
-				ans += d;
-			}
+	vector<long long> phi(MAXN);
+	vector<long long> bpf(MAXN, MAXN);
+	phi[0] = 0;
+	phi[1] = 1;
+	for (int i = 0; i < MAXN; i++)
+		phi[i] = i;
+
+	for (int i = 2; i < MAXN; i++) {
+		if (phi[i] == i) {
+			for (int j = i; j < MAXN; j += i)
+				phi[j] -= phi[j] / i, bpf[j] = i;
 		}
-		N[i] = ans;
+	}
+	
+
+	for (int& x : N){
+		long long ans = 0;
+
+		auto find_divisors = [&](auto&& find_divisors, long long v, long long m = 1){
+			if (v == 1){
+				if (m > 2)
+					ans += phi[m - 1];
+				return;
+			}
+			
+			long long p = bpf[v];
+			int count = 0;
+			while (bpf[v] == p)
+				v /= p, count++;
+			
+			for (int i = 0; i <= count; i++){
+				find_divisors(find_divisors, v, m);
+				m *= p;
+			}
+		};
+
+		find_divisors(find_divisors, x);
+
+		x = ans;
 	}
 }
