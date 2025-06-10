5d36b25bfd05085fc867c54661d278254cb84c04
OgradL <dragoleonardo1@gmail.com>
2025-06-10 17:20:32 +0200

    voli

--- voli/voli.cpp
+++ voli/voli.cpp
@@ -1,49 +1,61 @@
-#include <iostream>
+#include <functional>
 #include <vector>
 #include <queue>
 #include <unordered_map>
 using namespace std;
 
-struct edge{
-	long long A, B, L, C;
-
-	bool operator<(const edge& b) const{
-		return L > b.L;
-	}
-};
+using pll = pair<long long, long long>;
 
 long long plan(int N, int M, int K, vector<int> A, vector<int> B, vector<int> L, vector<int> C, vector<int> T) {
 
-	vector<vector<edge>> G(N);
+	++K;
+
+	unordered_map<long long, int> seen;
+
+	unordered_map<long long, vector<pair<long long, long long>>> G;
 	for (int i = 0; i < M; i++){
-		G[A[i]].push_back((edge){A[i], B[i], L[i], C[i]});
-		G[B[i]].push_back((edge){B[i], A[i], L[i], C[i]});
+		long long n0 = A[i] * 1LL * K + C[i] + 1;
+		long long n1 = B[i] * 1LL * K + C[i] + 1;
+
+		if (!seen[n0]){
+			seen[n0] = 1;
+			long long par = A[i] * 1LL * K;
+			G[par].push_back({n0, T[C[i]]});
+			G[n0].push_back({par, 0});
+		}
+		if (!seen[n1]){
+			seen[n1] = 1;
+			long long par = B[i] * 1LL * K;
+			G[par].push_back({n1, T[C[i]]});
+			G[n1].push_back({par, 0});
+		}
+
+		G[n0].push_back({n1, L[i]});
+		G[n1].push_back({n0, L[i]});
 	}
 
-	priority_queue<edge> pq;
-	pq.push((edge){0, 0, 0, -1});
+	priority_queue<pll, vector<pll>, greater<pll>> pq;
+	pq.push({0, 0});
 
-	vector<unordered_map<int, int>> dist(N);
-	vector<long long> ans(N, 1e18);
-	// vector<int> v(N, 0);
-	
+	unordered_map<long long, long long> dist;
+	unordered_map<long long, int> visited;
+	dist[0] = 0;
 
 	while (!pq.empty()){
-		auto [n, z, d, c] = pq.top();
+		auto [d, n] = pq.top();
 		pq.pop();
 
-		ans[n] = min(ans[n], d);
-		if (n == N-1) break;
-		if (dist[n][c]) continue;
-		dist[n][c] = 1;
-		// v[n] = 1;
+		if (n == K*1LL*(N-1)) break;
+		if (visited[n]) continue;
+		visited[n] = 1;
 
-		for (auto x : G[n]){
-			if (!dist[x.B][x.C] /*&& !v[x.B]*/){
-				pq.push((edge){x.B, 0, d + x.L + ((c != x.C) ? T[x.C] : 0), x.C});
+		for (auto [x, w] : G[n]){
+			if (!dist.count(x) || (dist[x] > d + w)){
+				dist[x] = d + w;
+				pq.push({d + w, x});
 			}
 		}
 	}
 
-	return ans[N-1];
+	return dist[(N-1)*1LL*K];
 }
