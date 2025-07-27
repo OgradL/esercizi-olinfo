ce6c6d7d5b8ac7a6efb7bab3413194f9ce8cb31e
OgradL <dragoleonardo1@gmail.com>
2025-07-27 17:30:01 +0200

    pokemongo

--- pokemongo/grader.cpp
+++ pokemongo/grader.cpp
@@ -10,46 +10,46 @@ void init(int, int);
 int new_pokemon();
 
 bool attack(int P) {
-	if (P < 0 || P >= N || life_left == 0) {
-		fprintf(fout, "Illecit call to function attack\n");
-		exit(0);
-	}
+    if (P < 0 || P >= N || life_left == 0) {
+        fprintf(fout, "Illecit call to function attack\n");
+        exit(0);
+    }
 
-	++count_attack;
-	if(count_attack > N) {
-		fprintf(fout, "Function attack called too many times\n");
-		exit(0);
-	}
-	if (P < S) return true;
-	--life_left;
-	return false;
+    ++count_attack;
+    if(count_attack > N) {
+        fprintf(fout, "Function attack called too many times\n");
+        exit(0);
+    }
+    if (P < S) return true;
+    --life_left;
+    return false;
 }
 
 int main() {
-	// fin = fopen("input.txt", "r");
-	// fout = fopen("output.txt", "w");
+    // fin = fopen("input.txt", "r");
+    // fout = fopen("output.txt", "w");
 	fin = stdin;
 	fout = stdout;
 
-	assert(fscanf(fin, "%d%d%d", &N, &K, &Q) == 3);
-	ans = (int *)malloc(Q * sizeof(int));
-	calls = (int *)malloc(Q * sizeof(int));
+    assert(fscanf(fin, "%d%d%d", &N, &K, &Q) == 3);
+    ans = (int *)malloc(Q * sizeof(int));
+    calls = (int *)malloc(Q * sizeof(int));
 
-	init(N, K);
+    init(N, K);
 
-	for (int i = 0; i < Q; i++) {
-		assert(fscanf(fin, "%d", &S) == 1);
-		life_left = K;
-		count_attack = 0;
-		ans[i] = new_pokemon();
-		calls[i] = count_attack;
-	}
+    for (int i = 0; i < Q; i++) {
+        assert(fscanf(fin, "%d", &S) == 1);
+        life_left = K;
+        count_attack = 0;
+        ans[i] = new_pokemon();
+        calls[i] = count_attack;
+    }
 
-	for (int i = 0; i < Q; i++)
-		fprintf(fout, "%d %d\n", ans[i], calls[i]);
+    for (int i = 0; i < Q; i++)
+        fprintf(fout, "%d %d\n", ans[i], calls[i]);
 
-	fclose(fin);
-	fclose(fout);
+    fclose(fin);
+    fclose(fout);
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
--- pokemongo/pokemongo.cpp
+++ pokemongo/pokemongo.cpp
@@ -1,17 +1,61 @@
-#include <iostream>
+#include <vector>
 using namespace std;
 
 bool attack(int);
 
 int n, k;
-void init(int N, int K){
+vector<vector<int>> cnt_moves, pre;
+
+
+void init(int N, int K) {
 	n = N;
 	k = K;
+
+	cnt_moves.assign(K + 1, vector<int>(N + 1, -1));
+	pre.assign(K + 1, vector<int>(N + 1, -1));
+
+	// cnt_moves[0].assign(N + 1, 1);
+	pre[0].assign(N + 1, 0);
+	pre[1].assign(N + 1, 0);
+	for (int i = 0; i <= N; i++){
+		cnt_moves[1][i] = i;
+	}
+
+	for (int i = 2; i <= K; i++){
+		int i0 = 1;
+		cnt_moves[i][1] = 1;
+		pre[i][1] = 0;
+		int last = 1;
+		for (int j = 2; j <= N; j++){
+			while (i0 && cnt_moves[i-1][i0] == cnt_moves[i][last])
+				i0++;
+			if (i0 + last < j){
+				last = j-1;
+			}
+			cnt_moves[i][j] = cnt_moves[i][last] + 1;
+			pre[i][j] = i0 - 1;
+		}
+	}
 }
 
-int new_pokemon(){
-	
+int new_pokemon() {
+	int lo = 0, hi = n, mid;
+	int lives = k;
 
+	while (lo + 1 < hi){
+		mid = lo + pre[lives][hi - lo];
+		mid = max(0, min(n-1, mid));
 
+		if (attack(mid)){
+			lo = mid + 1;
+		} else {
+			hi = mid;
+			lives--;
+		}
+	}
 
+	if (lo < hi && attack(lo))
+		return hi;
+
+	return lo;
 }
