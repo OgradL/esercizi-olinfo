47b1fb21f39573ee25ee53d94758d2298f7cee76
OgradL <dragoleonardo1@gmail.com>
2026-02-20 22:45:10 +0100

    decathlon

---
+++ decathlon/decathlon.cpp
@@ -0,0 +1,5 @@
+int salta(int X, int Y) {
+	if (Y >= X)
+		return 0;
+	return 500 + X - Y;
+}
---
+++ decathlon/grader.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int salta(int X, int Y);
+
+int main() {
+    int X, Y; cin >> X >> Y;
+    cout << salta(X, Y) << '\n';
+}
